6e1e2531224019ad1e47f2dc851dafc207f727f9
Pobitrodeb <pobitrodn@gmail.com>
2022-11-21 22:49:08 +0600

    construct argument pass

--- 10- Constructor Overriding/index.php
+++ 10- Constructor Overriding/index.php
@@ -1,1 +1,21 @@
-<?php 
+<?php
+
+class User {
+
+    public $name, $email; 
+
+    public function __construct($name, $email)
+    {
+        $this->name     = $name; 
+        $this->email    = $email; 
+    }
+
+    public function show()
+    {
+        echo "The User Name is : {$this->name}". '<br/>'; 
+        echo "The Email Address is : {$this->email}". '<br/>'; 
+    }
+}
+
+$suser = new User('Pobiro Debnath', 'pobitrodn@gmail.com'); // the function argument in this pass coz the user class define construct function 
+$suser->show();
