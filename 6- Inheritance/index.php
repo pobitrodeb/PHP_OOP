a69c6a14c854c9cd74312c251d3bbb2fe43322d8
Pobitrodeb <pobitrodn@gmail.com>
2022-11-21 18:49:08 +0600

    inheritance session commplete

--- 6- Inheritance/index.php
+++ 6- Inheritance/index.php
@@ -1,7 +1,7 @@
 <?php 
 
 class Bike{
-    public $brandName; 
+    public $brandName, $cyecleName; 
 
     function MotorCycle()
     {
@@ -13,12 +13,21 @@ class Bus extends Bike{    // extend means Bike class er sob kichu Bus class pab
 
 }
 
+class Cycle extends Bike{  // extend means Bike class er sob kichu Cycle class pabe
+
+}
 
 $bike = new Bike(); 
 $bike->brandName = 'Pulsure'; 
 echo $bike->brandName . '<br/>'; 
 
-// Inheritance 
+// Inheritance 1
 $bus = new Bus (); 
 $busMotorCycle =  $bus->MotorCycle(); 
 echo $busMotorCycle;
+
+// Inheritance 2
+echo '<br/>';
+$cyecle = new Bike(); 
+ $cyelceNameDeclear = $cyecle->cyecleName = 'Fonix'; 
+echo $cyelceNameDeclear;
