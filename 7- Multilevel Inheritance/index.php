2cba380a19f40fa12605cf77293f59b988082e7c
Pobitrodeb <pobitrodn@gmail.com>
2022-11-21 19:30:45 +0600

    multi level inheritance session complete

--- 7- Multilevel Inheritance/index.php
+++ 7- Multilevel Inheritance/index.php
@@ -1,1 +1,57 @@
 <?php 
+
+class HeadMaster {
+   function addNumber()
+   {
+     $x = 29; 
+     $y = 43; 
+     $result  = $x + $y; 
+     return $result; 
+   }
+   function multiNumber()
+   {
+    $x = 29; 
+     $y = 43; 
+     $result  = $x * $y; 
+     return $result; 
+   }
+}
+
+class AssistenTeacher extends HeadMaster
+{
+    function subNumber()
+    {
+      $x = 69; 
+      $y = 43; 
+      $result  = $x - $y; 
+      return $result; 
+    }
+     function modulesResult()
+    {
+        $x = 60; 
+        $y = 25; 
+        $result  = $x % $y; 
+        return $result; 
+    }
+}
+
+class classTeacher extends AssistenTeacher{
+
+}
+
+
+$addNumber = new HeadMaster(); 
+echo $addNumber->addNumber(); 
+
+
+echo '<br/>'; 
+$subNummber = new AssistenTeacher(); 
+echo $subNummber->subNumber();
+echo '<br/>';
+echo 'Extends of Head Master Class : ' . $subNummber->multiNumber();
+
+echo '<br/>';
+$classTeacher = new classTeacher(); 
+echo 'Extends of Assistent Teacher Class : ' . $classTeacher->modulesResult();
+echo '<br/>';
+echo 'Extends of Headmaster Class : ' . $classTeacher->multiNumber();
