054bb0f1f5899c44a5263659e36c8af1fe523085
Pobitrodeb <pobitrodn@gmail.com>
2022-11-21 20:10:34 +0600

    public fuction with public propertey access

--- 8- Access Modifiers/index.php
+++ 8- Access Modifiers/index.php
@@ -1,2 +1,15 @@
 <?php 
+// PUBLIC Function with Public Property 
+class User 
+{
+    public $name, $email; 
 
+    function getUser()
+    {
+        $this->name = 'Pobitro Debnath';
+        echo 'The User Name is : ' . $this->name ; 
+    }
+}
+
+$user = new User(); 
+$user->getUser();
