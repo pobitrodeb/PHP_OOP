ca795384c169e7af0ac8004320582acfb0047b58
Pobitrodeb <pobitrodn@gmail.com>
2022-11-21 20:28:35 +0600

    privet access

--- 8- Access Modifiers/index.php
+++ 8- Access Modifiers/index.php
@@ -8,8 +8,33 @@ class User
     {
         $this->name = 'Pobitro Debnath';
         echo 'The User Name is : ' . $this->name ; 
+        $this->getModarator();
     }
+
+     function getModarator()   
+      {
+          echo '<br/>';
+          echo 'This is Get Modarator Access';
+      }
 }
 
 $user = new User(); 
 $user->getUser();
+
+// Privet Function with Privete Property 
+
+class Modarator {
+    private $playrName, $accessType; 
+
+  // private function getModarator()   
+   function getModarator()   
+    {
+        $this->playrName = 'Liton Das'; 
+        echo $this->playrName;     
+       
+    }
+}
+
+$modarator  = new Modarator();
+echo '<br/>'; 
+$modarator->getModarator();
