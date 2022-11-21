3587a4b2addfc228d255e4909996bb49e9b82fed
Pobitrodeb <pobitrodn@gmail.com>
2022-11-21 16:33:21 +0600

    essinaly call to  function

--- 5- Class Constructor and Destructor/index.php
+++ 5- Class Constructor and Destructor/index.php
@@ -1,1 +1,13 @@
 <?php 
+class Book {
+    public $bookName; 
+
+    function bookName()
+    {
+        echo "This book name is : {$this->bookName}"; 
+    }
+ }
+
+ $book = new Book(); 
+ $book->bookName = 'PHP Web Programing'; 
+ echo $book->bookName;
