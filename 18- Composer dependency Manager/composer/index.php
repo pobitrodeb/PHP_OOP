5981b559d612265d96115f4ff3a13ddba1e5c519
Pobitrodeb <pobitrodn@gmail.com>
2022-11-22 21:41:56 +0600

    composer install, autoload innitiliaze, call to name space and sshow a message by index file

---
+++ 18- Composer dependency Manager/composer/index.php
@@ -0,0 +1,7 @@
+<?php 
+use App\Main;           // App namespace er Main Calss e call kora hoyche 
+require 'src/app/Main.php'; // Main.php file er sob kichu access pawar jonno require kora hoyche
+
+
+$message = new Main(); 
+$message->congratualtions();
---
+++ 18- Composer dependency Manager/composer/src/app/Main.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace App;
+
+class Main {
+
+    public function congratualtions(Type $var = null)
+    {
+        echo 'Congratulations on your success'; 
+    }
+}
